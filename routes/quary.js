38bc51c4f7d4aea2fb8862773f572c41d5b9b91a
boazfr1 <boazfr1@gmail.com>
2024-05-08 13:18:55 +0300

    remove all the my sql staff

--- routes/quary.js
+++ routes/quary.js
@@ -9,35 +9,6 @@ router.get('/', function(req, res, next) {
   );
 });
 
-router.get('/greetings', function (req, res) {
-    con.query("SELECT * FROM greeting", function (err, result) {
-        if (err) {
-            console.log("Error fetching greetings:", err);
-            res.status(500).json({ error: "Failed to fetch greetings" });
-            return;
-        }
-        console.log("Greetings fetched successfully:", result);
-        res.json(result);
-    });
-});
 
-router.post('/insert', function (req, res) {
-    console.log("req.body:", req.body);
-    let sql = `INSERT INTO greeting (text) VALUES
-    ('Hello, world!'),
-    ('Welcome to our website.'),
-    ('Greetings from OpenShift OKD.'),
-    ('Have a great day!'),
-    ('This is some random data.');
-    `
-    con.query(sql, function (err, greet) {
-        console.log("greet:", greet);
-        if (err) {
-            console.log(err);
-            res.send(JSON.stringify({ "answer": greet, "bool": false }))
-        };
-        res.send(JSON.stringify({ "answer": greet, "bool": true }));
-    })
-});
 
 module.exports = router;
